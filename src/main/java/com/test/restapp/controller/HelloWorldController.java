9816b629e29604abb6a88b5082c9644e9d3cac4c
techprograms <tech.gpr@gmail.com>
2023-02-05 23:40:51 +0530

    hello world example

---
+++ src/main/java/com/test/restapp/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.test.restapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
